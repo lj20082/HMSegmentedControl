caf2e98570949f324581f07cc57ffaf0502879b9
673637753@qq.com <673637753@qq.com>
2018-05-07 14:08:33 +0800

    暴露- (void)setSelectedSegmentIndex:(NSUInteger)index animated:(BOOL)animated notify:(BOOL)notify方法供使用

--- HMSegmentedControl/HMSegmentedControl.h
+++ HMSegmentedControl/HMSegmentedControl.h
@@ -256,6 +256,7 @@ typedef NS_ENUM(NSInteger, HMSegmentedControlImagePosition) {
 - (id)initWithSectionImages:(NSArray<UIImage *> *)sectionImages sectionSelectedImages:(NSArray<UIImage *> *)sectionSelectedImages;
 - (instancetype)initWithSectionImages:(NSArray<UIImage *> *)sectionImages sectionSelectedImages:(NSArray<UIImage *> *)sectionSelectedImages titlesForSections:(NSArray<NSString *> *)sectiontitles;
 - (void)setSelectedSegmentIndex:(NSUInteger)index animated:(BOOL)animated;
+- (void)setSelectedSegmentIndex:(NSUInteger)index animated:(BOOL)animated notify:(BOOL)notify;
 - (void)setIndexChangeBlock:(IndexChangeBlock)indexChangeBlock;
 - (void)setTitleFormatter:(HMTitleFormatterBlock)titleFormatter;
 
